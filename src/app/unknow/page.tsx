d9de6edd0e209a61f18996a0d38bc4b4cb35263d
Theo Kenzo de Araújo <theokenzo35@gmail.com>
2025-05-08 22:42:35 -0300

    resolve vercel problems

--- src/app/unknow/page.tsx
+++ src/app/unknow/page.tsx
@@ -1,3 +1,31 @@
-export function NotFoundOrUnauthorized() {
-  return <div></div>;
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function NotFoundOrUnauthorized() {
+  const router = useRouter();
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      router.push("/");
+    }, 5000);
+
+    const dotInterval = setInterval(() => {
+      setDots((prev) => (prev.length < 5 ? prev + " . " : ""));
+    }, 1000);
+
+    return () => {
+      clearTimeout(redirectTimeout);
+      clearInterval(dotInterval);
+    };
+  }, [router]);
+
+  return (
+    <main className="flex flex-col items-center justify-center w-full min-h-screen">
+      <p>You shouldn't be here{dots}</p>
+      <p className="font-semibold">{`ʘᆽʘ`}</p>
+    </main>
+  );
 }
